df0a54a899922caaaaa27d0e88576096f407f2d4
Joshua Rubin <me@jawa.dev>
2020-12-16 10:57:50 -0700

    latest updates
    Signed-off-by: Joshua Rubin <me@jawa.dev>

--- keyboards/gray_studio/space65/space65.h
+++ keyboards/gray_studio/space65/space65.h
@@ -25,6 +25,7 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
+// clang-format off
 #define LAYOUT( \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D, k0E, k0F, \
     k10,      k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, k1E, k1F, \
@@ -55,6 +56,21 @@
     { k40, k41,   KC_NO, k43, KC_NO, KC_NO, KC_NO, k47, KC_NO, KC_NO, k4A, k4B, KC_NO, k4D,   k4E,   k4F }, \
 }
 
+#define LAYOUT_65_ansi_blocker_split_bs( \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D, k0E, k0F, \
+    k10,      k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, k1E, k1F, \
+    k20,      k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k2C, k2D,      k2F, \
+    k30,      k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3D,      k3E, k3F, \
+    k40, k41,      k43,                k47,           k4A, k4B,      k4D, k4E, k4F  \
+) \
+{ \
+    { k00, k01,   k02,   k03, k04,   k05,   k06,   k07, k08,   k09,   k0A, k0B, k0C,   k0D,   k0E,   k0F }, \
+    { k10, KC_NO, k12,   k13, k14,   k15,   k16,   k17, k18,   k19,   k1A, k1B, k1C,   k1D,   k1E,   k1F }, \
+    { k20, KC_NO, k22,   k23, k24,   k25,   k26,   k27, k28,   k29,   k2A, k2B, k2C,   k2D,   KC_NO, k2F }, \
+    { k30, KC_NO, k32,   k33, k34,   k35,   k36,   k37, k38,   k39,   k3A, k3B, KC_NO, k3D,   k3E,   k3F }, \
+    { k40, k41,   KC_NO, k43, KC_NO, KC_NO, KC_NO, k47, KC_NO, KC_NO, k4A, k4B, KC_NO, k4D,   k4E,   k4F }, \
+}
+
 #define LAYOUT_65_iso_blocker( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C,    K0D,   K0F,   \
     K10,   K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,         K1F,   \
@@ -68,3 +84,4 @@
     { K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   K3B,   KC_NO, K3D,   K3E,   K3F   }, \
     { K40,   K41,   KC_NO, K43,   KC_NO, KC_NO, KC_NO, K47,   KC_NO, KC_NO, K4A,   K4B,   KC_NO, K4D,   K4E,   K4F   }, \
 }
+// clang-format on
--- users/joshuarubin/joshuarubin.h
+++ users/joshuarubin/joshuarubin.h
@@ -10,6 +10,7 @@ enum custom_keycodes {
 #ifdef UNICODE_ENABLE
 #    define KC_NDSH UC(0x2013)
 #    define KC_MDSH UC(0x2014)
+#    define KC_ELIP UC(0x2026)
 #else
 #    define KC_NDSH KC_NO
 #    define KC_MDSH KC_NO
